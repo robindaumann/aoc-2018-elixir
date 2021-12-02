33359a9475d657b3f8975fee7d704276911547b6
Robin Daumann <r.daumann@deutsche-glasfaser.de>
2021-12-02 11:34:58 +0100

    remove input

--- lib/Tree.ex
+++ lib/Tree.ex
@@ -1,27 +1,29 @@
 defmodule Tree do
   defstruct children: [], meta: []
 
-  @spec leaf([number]) :: Tree.t()
+  @type t :: %Tree{children: [t], meta: [number()]}
+
+  @spec leaf([number]) :: t
   def leaf(meta) do
     %Tree{children: [], meta: meta}
   end
 
-  @spec node([Tree.t()], [number]) :: Tree.t()
+  @spec node([t], [number]) :: t
   def node(children, meta) do
     %Tree{children: children, meta: meta}
   end
 
-  @spec sum(Tree.t()) :: number()
+  @spec sum(t) :: number()
   def sum(%Tree{children: [], meta: meta}) do
     Enum.sum(meta)
   end
 
   def sum(%Tree{children: children, meta: meta}) do
     s = Enum.sum(meta)
-     Enum.reduce(children, s, fn x, acc -> sum(x) + acc end)
+    Enum.reduce(children, s, fn x, acc -> sum(x) + acc end)
   end
 
-  @spec value(Tree.t()) :: number()
+  @spec value(t) :: number()
   def value(%Tree{children: [], meta: meta}) do
     Enum.sum(meta)
   end
@@ -29,7 +31,7 @@ defmodule Tree do
   def value(%Tree{children: children, meta: meta}) do
     meta
     |> Enum.reject(fn i -> i == 0 || i > length(children) end)
-    |> Enum.map(fn i -> Enum.fetch!(children, i-1) end)
+    |> Enum.map(fn i -> Enum.fetch!(children, i - 1) end)
     |> Enum.reduce(0, fn x, acc -> value(x) + acc end)
   end
 end
